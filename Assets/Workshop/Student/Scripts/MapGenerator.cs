220107837c9d7f272152507728a1e51e4da5a4b6
M4XY-art <amaxbuteneers666@gmail.com>
2026-08-25 13:10:03 +0700

    workshop done 🐶

--- Assets/Workshop/Student/Scripts/MapGenerator.cs
+++ Assets/Workshop/Student/Scripts/MapGenerator.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 namespace Workshop.Student
@@ -12,6 +13,10 @@ namespace Workshop.Student
         public GameObject[] wallTiles;
         public GameObject[] foodTiles;
 
+        public GameObject[] obstacleTiles;
+
+
+
         public string[,] saveItemMap = new string[3, 3] 
         {
             { " ", "Soda", " "},
@@ -20,23 +25,61 @@ namespace Workshop.Student
         };
 
         // 1. declare Players variable
-
+        public GameObject[] players;
         // 7. declare Exit variable 
-
+        public GameObject exit;
 
         public void Start()
-        {
-            // 1. random player at the position <0, 0> map
-            
-            // 2. create obstacles (in the middle)
 
+        {
+            
+
+            // 1. random player at the position <0, 0> map
+            int randomPlayer = UnityEngine.Random.Range(0, players.Length);
+            GameObject player = Instantiate(players[randomPlayer],new Vector2(0, 0),Quaternion.identity);
+            player.name = "Player";
+
+            // 2. create obstacles (in the middle in a straight line 4,0 to 4,4)
+            bool IsObstacle(int x, int y)
+            {
+                // Vertical obstacle from (4,0) to (4,4)
+                if (x == 4 && y >= 0 && y <= 4)
+                {
+                    return true;
+                }
+
+                return false;
+            }
             // 3. create floor
             for (int y = 0; y < rows; y++)
             {
                 for (int x = 0; x < columns; x++)
                 {
-                    int r = UnityEngine.Random.Range(0, floorTiles.Length);
-                    GameObject tile = Instantiate(floorTiles[r], new Vector2(x, y), Quaternion.identity);
+                    // If this position is an obstacle, create obstacle instead
+                    if (IsObstacle(x, y))
+                    {
+                        int r = UnityEngine.Random.Range(0, obstacleTiles.Length);
+
+                        GameObject obstacle = Instantiate(
+                            obstacleTiles[r],
+                            new Vector2(x, y),
+                            Quaternion.identity
+                        );
+
+                        obstacle.name = "Obstacle" + x + "_" + y;
+
+                        continue;
+                    }
+
+                    // Otherwise create normal floor
+                    int floorRandom = UnityEngine.Random.Range(0, floorTiles.Length);
+
+                    GameObject tile = Instantiate(
+                        floorTiles[floorRandom],
+                        new Vector2(x, y),
+                        Quaternion.identity
+                    );
+
                     tile.name = "Floor" + x + "_" + y;
                 }
             }
@@ -83,7 +126,11 @@ namespace Workshop.Student
                 }
             }
             // 7. place exit (create in the top right)
+            GameObject exitObject = Instantiate(exit,new Vector2(columns - 1, rows - 1),Quaternion.identity);
 
+            exitObject.name = "Exit";
+
+            
         }
     }
 
